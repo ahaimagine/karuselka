ce427691be036eb5afb604815527d473b6a0d728
Your Name <you@example.com>
2020-07-21 08:16:22 +0300

    heeelp

---
+++ js/main.js.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+});
+
+$(window).on("scroll" , function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+        $('a').addClass('black');
+        $('navbar-brand').addClass('black');
+        $('.navbar-light .navbar-toggler').addClass('black');
+    }
+
+    else {
+        $('nav').removeClass('black');
+        $('a').removeClass('black');
+        $('navbar-brand').removeClass('black');
+        $('.navbar-light .navbar-toggler').removeClass('black');
+    }
+});
+(function() {
+    const formEl = document.querySelector("#contactForm");
+    formEl.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const body = {
+            name: event.target.name.value,
+            email: event.target.email.value,
+            message: event.target.message.value
+        }
+
+        fetch("https://stoliari-node.herokuapp.com/api", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"  
+            },
+            body: JSON.stringify(body)
+        }).then(r => r.text()).then(response => {
+            // TODO add alert about successful submit 
+            console.log(response, "from server")
+        });
+    });
+})();
